97e56c55eb44dc423fa03006a76523d472ef9b4f
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-12-03 11:06:16 +0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000; // You can choose any port
+const port = process.env.PORT || 3000; // You can choose any port
 
 // Middleware to parse JSON requests
 app.use(express.json());
